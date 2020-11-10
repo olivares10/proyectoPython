9a2d8a8663db8da54512ebdc040cec8310f0f13c
olivares10 <leolivares89@gmail.com>
2020-11-09 19:44:45 -0600

    clase 3

---
+++ nombre.py
@@ -0,0 +1,5 @@
+nombre = input("Digite su nombre:")
+
+edad= int (input("Edad:"))
+edad= edad+10
+print("Su nombre es ",nombre"")
---
+++ prueba.py
@@ -0,0 +1,3 @@
+nombre= input ("indique su nombre :")
+saludo="hola: "+nombre
+print(saludo)
